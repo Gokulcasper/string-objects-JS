0e96e259f7fe14f23ef974e0b3a067b2bf565b91
Gokulcasper <ronitcasper215@gmail.com>
2022-06-04 20:17:43 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+let firstname = "Gokul";
+let lastname = "Raj";
+let para = "Hi This is Gokul! ";
+//or
+// const FirstName=new string("Gokul");
+
+console.log(typeof firstname);
+console.log(firstname.length);
+console.log(firstname.charAt(3));
+console.log(firstname.concat("", lastname));
+console.log(firstname.includes("ku"));
+console.log(firstname.startsWith("Go"));
+console.log(firstname.endsWith("ul"));
+console.log(firstname.indexOf("k"));
+console.log(firstname.repeat(10));
+console.log(firstname.replace("Gokul", "Deepak"));
+console.log(firstname.slice(3));
+console.log(firstname.toUpperCase());
+console.log(firstname.toLowerCase());
+console.log(para.split("", 2));
+console.log(para.split(" ", 2));
+console.log(para.substring(3));
